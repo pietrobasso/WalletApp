a7c598af714d6bc198aa3583919cc1a62aeef1f8
Pietro Basso <pietro.basso@rawfish.com>
2019-02-17 12:32:59 +0100

    Update Onboarding scene.

--- wallet/AppDependencies.swift
+++ wallet/AppDependencies.swift
@@ -25,6 +25,6 @@ class AppDependenciesProvider: AppDependencies {
     // MARK: - Managers 
     // Managers can depend on services 
     lazy var cardsManager: CardsManager = {
-        return MockedCardsManager(cardsCount: 5)
+        return MockedCardsManager(cardsCount: 25)
     }()
 }
